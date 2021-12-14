cf8768cc481a88f79ff101792139467eb531f3de
Andrey Beloboky <andrei.beloboky@yandex.ru>
2021-12-14 18:56:47 +0300

    copy task List1 and edit on the set

---
+++ src/com/company/Set1.java
@@ -0,0 +1,114 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class Set1 implements Set2 {
+
+    private String[] array;
+    private int setSize = 0;
+
+    public Set1() {
+        this.array = new String[10];
+    }
+
+    /**
+     * @param size - the size is entered by the user
+     */
+    public Set1(int size) {
+        this.array = new String[size];
+    }
+
+    /**
+     * @param data - the data is entered by the user;
+     */
+    @Override
+    public void add(String data) {
+        if (setSize == array.length) {
+            createNewArray();
+        }
+        array[setSize] = data;
+        int equal = 0;
+        for (int i = 0; i < setSize; i++) {
+            if (array[setSize].equals(array[i]) || array[i].hashCode() == array[setSize].hashCode()) {
+                equal++;
+            }
+        }
+        if (equal >= 1) {
+            array[setSize] = null;
+            setSize--;
+        }
+        setSize++;
+    }
+
+    /**
+     * @param index - who needs remove;
+     */
+    @Override
+    public void remove(int index) {
+        for (int i = index; i < setSize; i++) {
+            array[index] = array[index + 1];
+            index++;
+        }
+        if (setSize * 2 < array.length) {
+            decreaseArray();
+        }
+        setSize--;
+    }
+
+    /**
+     * @param index - the index is entered by the user.
+     * @return - inside in the index.
+     */
+    @Override
+    public boolean contains(int index) {
+        return false;
+    }
+
+    /**
+     * @return - number of filled elements;
+     */
+    @Override
+    public int size() {
+        return setSize;
+    }
+
+    /**
+     * @param index - the index to be changed
+     * @param data  - the element to be replaced;
+     */
+    @Override
+    public void set(int index, String data) {
+        array[index] = data;
+    }
+
+    private void createNewArray() {
+        String[] temp = array;
+        this.array = new String[temp.length * 2];
+        System.arraycopy(temp, 0, array, 0, temp.length);
+    }
+
+    public void decreaseArray() {
+        String[] temp = array;
+        this.array = new String[array.length - 10];
+        System.arraycopy(temp, 0, array, 0, array.length);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Set1 set1 = (Set1) o;
+        return Arrays.equals(array, set1.array);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(array);
+    }
+
+    public void print() {
+        for (int i = 0; i < setSize; i++) {
+            System.out.println(array[i]);
+        }
+    }
+}
---
+++ src/com/company/Set2.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public interface Set2 {
+    void add (String e);
+    void remove(int index);
+    boolean contains(int index);
+    int size();
+    void set(int index, String e);
+}
